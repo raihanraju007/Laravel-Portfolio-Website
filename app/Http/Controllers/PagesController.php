46e7db860d1539e1e397d8ff5a004c0a89739d38
raihanraju007 <raihanraju007@gmail.com>
2022-07-27 13:25:00 +0600

    Dynamic Background & Title, Sub Title Done

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -2,13 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Main;
 use Illuminate\Http\Request;
 
 class PagesController extends Controller
 {
     public function index()
     {
-        return view('pages.index');
+        $main = Main::first();
+        return view('pages.index',compact('main'));
     }
 
     public function dashboard()
